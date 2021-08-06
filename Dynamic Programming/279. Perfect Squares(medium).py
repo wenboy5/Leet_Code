e23008e0ab6e9dd507a535e22f6b98f28d423669
Wenbo Yang <37161906+wenboy5@users.noreply.github.com>
2021-08-06 17:12:54 +0000

    279

---
+++ Dynamic Programming/279. Perfect Squares(medium).py
@@ -0,0 +1,51 @@
+'''
+Given an integer n, return the least number of perfect square numbers that sum to n.
+
+A perfect square is an integer that is the square of an integer; in other words, it is the product of some integer with itself. For example, 1, 4, 9, and 16 are perfect squares while 3 and 11 are not.
+
+ 
+
+Example 1:
+
+Input: n = 12
+Output: 3
+Explanation: 12 = 4 + 4 + 4.
+Example 2:
+
+Input: n = 13
+Output: 2
+Explanation: 13 = 4 + 9.
+'''
+
+class Solution:
+    def numSquares(self, n):
+        dp= [math.inf] * (n+1)
+        dp[0] = 0
+        
+        for i in range(1,n+1):
+            j = 0
+            while (j*j<=i):
+                dp[i] = min(dp[i],dp[i-j*j]+1)
+                j+=1
+                
+        return dp[n]
+  
+class Solution(object):
+    def numSquares(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        square_nums = [i**2 for i in range(0, int(math.sqrt(n))+1)]
+        
+        dp = [float('inf')] * (n+1)
+        # bottom case
+        dp[0] = 0
+        
+        for i in range(1, n+1):
+            for square in square_nums:
+                if i < square:
+                    break
+                dp[i] = min(dp[i], dp[i-square] + 1)
+        
+        return dp[-1]
