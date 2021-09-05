be0720959d623c631528b403cb7ffd3f38850f9e
Wenbo Yang <37161906+wenboy5@users.noreply.github.com>
2021-09-05 16:33:09 +0000

    543

---
+++ Tree/543. Diameter of Binary Tree(easy).py
@@ -0,0 +1,69 @@
+'''
+Given the root of a binary tree, return the length of the diameter of the tree.
+
+The diameter of a binary tree is the length of the longest path between any two nodes in a tree. This path may or may not pass through the root.
+
+The length of a path between two nodes is represented by the number of edges between them.
+
+ 
+
+Example 1:
+
+
+Input: root = [1,2,3,4,5]
+Output: 3
+Explanation: 3 is the length of the path [4,2,1,3] or [5,2,1,3].
+Example 2:
+
+Input: root = [1,2]
+Output: 1
+'''
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def find_height(self,root):
+            if root == None:
+                return 0
+            else:
+                return 1 + max(self.find_height(root.left), self.find_height(root.right))
+            
+    def diameterOfBinaryTree(self, root: TreeNode) -> int: 
+            if root == None:
+                return 0
+
+            return max(self.find_height(root.left) + self.find_height(root.right), self.diameterOfBinaryTree(root.left), self.diameterOfBinaryTree(root.right))
+
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def diameterOfBinaryTree(self, root: TreeNode) -> int:
+        diameter = 0
+
+        def longest_path(node):
+            if not node:
+                return 0
+            nonlocal diameter
+            # recursively find the longest path in
+            # both left child and right child
+            left_path = longest_path(node.left)
+            right_path = longest_path(node.right)
+
+            # update the diameter if left_path plus right_path is larger
+            diameter = max(diameter, left_path + right_path)
+
+            # return the longest one between left_path and right_path;
+            # remember to add 1 for the path connecting the node and its parent
+            return max(left_path, right_path) + 1
+
+        longest_path(root)
+        return diameter
