5e86133fc2167ae867aa9d2158a3289edc20b94a
Wenbo Yang <37161906+wenboy5@users.noreply.github.com>
2021-09-10 03:47:23 +0000

    687

---
+++ Tree/687. Longest Univalue Path(medium).py
@@ -0,0 +1,66 @@
+'''
+Given the root of a binary tree, return the length of the longest path, where each node in the path has the same value. This path may or may not pass through the root.
+
+The length of the path between two nodes is represented by the number of edges between them.
+
+ 
+
+Example 1:
+
+
+Input: root = [5,4,5,1,1,5]
+Output: 2
+Example 2:
+
+
+Input: root = [1,4,5,4,4,5]
+Output: 2
+'''
+class Solution(object):
+    def longestUnivaluePath(self, root):
+        self.ans = 0
+
+        def arrow_length(node):
+            if not node: return 0
+            left_length = arrow_length(node.left)
+            right_length = arrow_length(node.right)
+            left_arrow = right_arrow = 0
+            if node.left and node.left.val == node.val:
+                left_arrow = left_length + 1
+            if node.right and node.right.val == node.val:
+                right_arrow = right_length + 1
+            self.ans = max(self.ans, left_arrow + right_arrow)
+            return max(left_arrow, right_arrow)
+
+        arrow_length(root)
+        return self.ans
+
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def longestUnivaluePath(self, root: Optional[TreeNode]) -> int:
+        ans = 0
+        
+        def check(root,val):
+            if not root:
+                return 0
+            elif root.val != val:
+                return 0
+            else:
+                return 1+ max(check(root.left,val), check(root.right,val))
+        
+        def re(root):
+            nonlocal ans
+            if root:
+                new = check(root.right,root.val) + check(root.left,root.val)
+                if new > ans:
+                    ans = new
+                re(root.left)
+                re(root.right)
+        re(root)
+        return ans
