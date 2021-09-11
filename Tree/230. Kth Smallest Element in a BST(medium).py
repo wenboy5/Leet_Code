c2b2bb8d8f786107189a5448768e228cc4b96520
Wenbo Yang <37161906+wenboy5@users.noreply.github.com>
2021-09-11 20:51:55 +0000

    230

---
+++ Tree/230. Kth Smallest Element in a BST(medium).py
@@ -0,0 +1,66 @@
+'''
+Given the root of a binary search tree, and an integer k, return the kth (1-indexed) smallest element in the tree.
+
+ 
+
+Example 1:
+
+
+Input: root = [3,1,4,null,2], k = 1
+Output: 1
+Example 2:
+
+
+Input: root = [5,3,6,2,4,null,null,1], k = 3
+Output: 3
+'''
+
+class Solution:
+    def kthSmallest(self, root, k):
+        """
+        :type root: TreeNode
+        :type k: int
+        :rtype: int
+        """
+        stack = []
+        
+        while True:
+            while root:
+                stack.append(root)
+                root = root.left
+            root = stack.pop()
+            k -= 1
+            if not k:
+                return root.val
+            root = root.right
+
+class Solution:
+    def kthSmallest(self, root, k):
+        """
+        :type root: TreeNode
+        :type k: int
+        :rtype: int
+        """
+        def inorder(r):
+            return inorder(r.left) + [r.val] + inorder(r.right) if r else []
+    
+        return inorder(root)[k - 1]
+
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
+        ans = set()
+        def re(root):
+            if root != None:
+                ans.add(root.val)
+                re(root.left)
+                re(root.right)
+        re(root)
+        return sorted(ans)[k-1]
+
